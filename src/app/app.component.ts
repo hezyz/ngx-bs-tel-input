b32d764da5149cf15f9c71c4b59a74ef62a23e42
hezyziv <hezyz1@gmail.com>
2024-12-22 14:51:08 +0200

    upgarding cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 import { NgxBsTelInputComponent } from '../../projects/ngx-bs-tel-input/src/public-api';
 import { SearchCountryField } from '../../projects/ngx-bs-tel-input/src/lib/core/models/search-country-field.enum';
 import { CountryISO } from '../../projects/ngx-bs-tel-input/src/lib/core/models/country-iso.enum';
 import { PhoneNumberFormat } from '../../projects/ngx-bs-tel-input/src/lib/core/models/phone-number-format.enum';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule, NgxBsTelInputComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgxBsTelInputComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
